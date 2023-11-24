62cb9151dcdff4b697dec92fa7dd50c3d8b657e3
Faisal <Faisal-111@outlook.com>
2023-11-24 19:42:00 +0300

    Added database tables

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import tkinter
+import sqlite3
 from tkinter import ttk
+
+
+
+
+conn = sqlite3.connect("GolfDataBase.db")
+conn.execute('''CREATE TABLE IF NOT EXISTS UserData
+         (ID INT PRIMARY KEY NOT NULL,
+         FName TEXT NOT NULL,
+         LName TEXT NOT NULL,
+         UserClass TEXT NOT NULL,
+         Password TEXT NOT NULL,
+         Email TEXT NOT NULL,
+         PhoneNumber INT(10) NOT NULL);''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS CartData
+         (PlateNumber INT PRIMARY KEY NOT NULL,
+         College TEXT NOT NULL);''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS Reservations
+         (PlateNumber INT NOT NULL,
+         ID INT NOT NULL,
+         StartDate TIMESTAMP NOT NULL,
+         EndDate TIMESTAMP NOT NULL,
+         FOREIGN KEY(PlateNumber) REFERENCES CareData(PlateNumber),
+         FOREIGN KEY(ID) REFERENCES UserData(ID));''')
 
 class app:
     def init(self):
@@ -17,6 +43,6 @@ class app:
         self.main.mainloop()
 
 
-     def change_page(self, page_name):
-         self.current_page.frame.destroy()
-         self.current_page = page_name(self)
+     # def change_page(self, page_name):
+     #     self.current_page.frame.destroy()
+     #     self.current_page = page_name(self)
