f27f807b92d5e29fe0e9a16db67df5851b74b3a0
Faisal <Faisal-111@outlook.com>
2023-11-25 19:00:23 +0300

    Added Insertion to creating carts, changed the college Entry into an OptionBox (Combobox), made it so the logout button will transfer the user to the Logion window

--- Admin.py
+++ Admin.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import csv  # For CSV backup (you may need to install the 'csv' module)
+import sqlite3
 
 class AdminWindow:
     def __init__(self, master):
@@ -11,13 +12,15 @@ class AdminWindow:
         tk.Label(master, text="Golf Cart Plate Number:").grid(row=0, column=0, sticky="e")
         tk.Label(master, text="College:").grid(row=1, column=0, sticky="e")
 
-        # Entry widgets
         self.plate_number_entry = tk.Entry(master)
-        self.college_entry = tk.Entry(master)
+        self.selected_college = tk.StringVar()
+        colleges = ["College of Computer and Information Sciences", "College of Science", "College of Engineering",
+                    "College of Business Administration"]
+        self.college_menu = tk.OptionMenu(self.master, self.selected_college, *colleges)
 
         # Grid layout for entry widgets
         self.plate_number_entry.grid(row=0, column=1)
-        self.college_entry.grid(row=1, column=1)
+        self.college_menu.grid(row=1, column=1)
 
         # Buttons
         tk.Button(master, text="Create", command=self.create_record).grid(row=2, column=0, columnspan=2)
@@ -25,21 +28,26 @@ class AdminWindow:
         tk.Button(master, text="Backup", command=self.backup).grid(row=4, column=0, columnspan=2)
 
     def create_record(self):
-        # Dummy: Send information to the central database (replace with actual database interaction)
-        plate_number = self.plate_number_entry.get()
-        college = self.college_entry.get()
+        # Dummy: Send information to the central database (replace with actual database interaction)     (((DONE)))
 
-        # Dummy: Print information (replace with actual database insertion)
-        print(f"Record Created:\nPlate Number: {plate_number}\nCollege: {college}")
+        conn = sqlite3.connect("GolfDataBase.db")
+        try:
+            insertion = "INSERT INTO CartData (PlateNumber, College) VALUES (?, ?)"
+            insertionValues = (self.plate_number_entry.get(), self.selected_college.get())
+            conn.execute(insertion, insertionValues)
+            conn.commit()
+        except sqlite3.IntegrityError:
+            messagebox.showerror("Error", "Plate Number Already Exists!!")
+        conn.close()
 
     def logout(self):
-        # Add logic to go back to the sign-up window or login window
-        pass
+        self.master.destroy()
+        import Login
+        Login.LoginWindow(self.master)
 
     def backup(self):
-        # Dummy: Backup information to a CSV file (replace with actual database backup)
         data_to_backup = [["Plate Number", "College"],
-                          [self.plate_number_entry.get(), self.college_entry.get()]]
+                          [self.plate_number_entry.get(), self.selected_college.get()]]
 
         with open('backup.csv', 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
