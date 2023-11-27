32bae6fb5bfba9f2e73509b49161eb9a5281c681
Faisal <Faisal-111@outlook.com>
2023-11-27 12:17:31 +0300

    Added inpout validations for the user class and the ID based on the user class, also improved the GUI of the Signup window

--- Signup.py
+++ Signup.py
@@ -9,35 +9,47 @@ import Login
 class SignUpWindow:
     def __init__(self, main):
         self.main = main
-        self.frame = tk.Frame(self.main)
+        self.main.title("Sign Up")
+        self.main.geometry("500x500")
+        self.main.configure(bg="#add8e6")  # Hexadecimal code for light blue
+        self.main.columnconfigure(0, weight=1)
+        self.main.rowconfigure(0, weight=1)
+
+        welcome_font = ("Helvetica", 20, "bold")
+        font = ("Helvetica", 12)
+        light_blue = "#add8e6"
+
+        self.frame = tk.Frame(self.main, bg="#add8e6")
         self.frame.grid()
 
-        tk.Label(self.frame, text="First Name:").grid(row=0, column=0, sticky="e")
-        tk.Label(self.frame, text="Last Name:").grid(row=1, column=0, sticky="e")
-        tk.Label(self.frame, text="User Class:").grid(row=2, column=0, sticky="e")
-        tk.Label(self.frame, text="Student ID:").grid(row=3, column=0, sticky="e")
-        tk.Label(self.frame, text="Password:").grid(row=5, column=0, sticky="e")
-        tk.Label(self.frame, text="Email:").grid(row=6, column=0, sticky="e")
-        tk.Label(self.frame, text="Phone Number:").grid(row=7, column=0, sticky="e")
+        tk.Label(self.frame, text="        Welcome to KSU Golf", font=welcome_font, bg=light_blue).grid(row=0, column=0, columnspan=2, pady=20)
+        tk.Label(self.frame, text="First Name:", font=font, bg=light_blue).grid(row=1, column=0, sticky="e", padx=3, pady=5)
+        tk.Label(self.frame, text="Last Name:", font=font, bg=light_blue).grid(row=2, column=0, sticky="e", padx=3, pady=5)
+        tk.Label(self.frame, text="User Class:", font=font, bg=light_blue).grid(row=3, column=0, sticky="e", padx=3, pady=5)
+        tk.Label(self.frame, text="Student ID:", font=font, bg=light_blue).grid(row=4, column=0, sticky="e", padx=3, pady=5)
+        tk.Label(self.frame, text="Password:", font=font, bg=light_blue).grid(row=5, column=0, sticky="e", padx=3, pady=5)
+        tk.Label(self.frame, text="Email:", font=font, bg=light_blue).grid(row=6, column=0, sticky="e", padx=3, pady=5)
+        tk.Label(self.frame, text="Phone Number:", font=font, bg=light_blue).grid(row=7, column=0, sticky="e", padx=3, pady=5)
 
-        self.first_name_entry = tk.Entry(self.frame)
-        self.last_name_entry = tk.Entry(self.frame)
-        self.user_class_entry = tk.Entry(self.frame)
-        self.id_entry = tk.Entry(self.frame)
-        self.password_entry = tk.Entry(self.frame, show="*")  # Hide password
-        self.email_entry = tk.Entry(self.frame)
-        self.phone_entry = tk.Entry(self.frame)
+        self.first_name_entry = tk.Entry(self.frame, font=font)
+        self.last_name_entry = tk.Entry(self.frame, font=font)
+        self.user_class_entry = tk.Entry(self.frame, font=font)
+        self.id_entry = tk.Entry(self.frame, font=font)
+        self.password_entry = tk.Entry(self.frame, show="*", font=font)  # Hide password
+        self.email_entry = tk.Entry(self.frame, font=font)
+        self.phone_entry = tk.Entry(self.frame, font=font)
 
-        self.first_name_entry.grid(row=0, column=1)
-        self.last_name_entry.grid(row=1, column=1)
-        self.user_class_entry.grid(row=2, column=1)
-        self.id_entry.grid(row=3, column=1)
-        self.password_entry.grid(row=5, column=1)
-        self.email_entry.grid(row=6, column=1)
-        self.phone_entry.grid(row=7, column=1)
+        self.first_name_entry.grid(row=1, column=1, pady=5)
+        self.last_name_entry.grid(row=2, column=1, pady=5)
+        self.user_class_entry.grid(row=3, column=1, pady=5)
+        self.id_entry.grid(row=4, column=1, pady=3)
+        self.password_entry.grid(row=5, column=1, pady=5)
+        self.email_entry.grid(row=6, column=1, pady=5)
+        self.phone_entry.grid(row=7, column=1, pady=5)
 
-        tk.Button(self.frame, text="Submit", command=self.submit).grid(row=8, column=0, columnspan=2)
-        tk.Button(self.frame, text="Login", command=self.login).grid(row=9, column=0, columnspan=2)
+        tk.Button(self.frame, text="Submit", command=self.submit, font=font).grid(row=8, column=0, columnspan=2, pady=15)
+        tk.Label(self.frame, text="Already Have An Account?", font=font, bg=light_blue).grid(row=9, column=1, sticky="w", padx=1)
+        tk.Button(self.frame, text="Login", command=self.login, font=font).grid(row=9, column=2, sticky="e", columnspan=1, pady=1)
 
     def submit(self):
         if not self.validate_input():
@@ -51,6 +63,7 @@ class SignUpWindow:
             insertionValues = (self.id_entry.get(), self.first_name_entry.get(), self.last_name_entry.get(), self.user_class_entry.get(), hashed_password, self.email_entry.get(), self.phone_entry.get())
             conn.execute(insertion, insertionValues)
             conn.commit()
+            messagebox.showinfo("Success", "Your Account Has Been Created Successfully!.")
         except sqlite3.IntegrityError:
             messagebox.showerror("Error", "Account Already Exists!")
         conn.close()
@@ -72,12 +85,20 @@ class SignUpWindow:
             messagebox.showerror("Error", "Invalid phone number format.")
             return False
 
-        return True
+        # Check for valid user classes
+        user_class = self.user_class_entry.get().lower()
+        valid_user_classes = ["student", "faculty", "employee", "admin"]
+        if user_class not in valid_user_classes:
+            messagebox.showerror("Error", "Invalid user class. Please choose Student, Faculty, Employee, or Admin.")
+            return False
 
-#
-# # Create the main application window
-# root = tk.Tk()
-# app = SignUpWindow(root)
-#
-# # Run the application
-# root.mainloop()
+        # ID validation based on user class
+        if user_class == "student" and not (self.id_entry.get().isdigit() and len(self.id_entry.get()) == 10):
+            messagebox.showerror("Error", "For students, ID must be 10 digits.")
+            return False
+        elif user_class in ["faculty", "employee"] and not (
+                self.id_entry.get().isdigit() and len(self.id_entry.get()) == 6):
+            messagebox.showerror("Error", "For Faculty and Employees, ID must be 6 digits.")
+            return False
+
+        return True
