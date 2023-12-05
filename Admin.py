d7fb6da1703d3030765542db88b4c67699a4975e
Faisal <Faisal-111@outlook.com>
2023-12-05 15:45:04 +0300

    Improved the GUI of the Admin Window, and modified the backup method

--- Admin.py
+++ Admin.py
@@ -9,35 +9,31 @@ class AdminWindow:
         self.main = main
         self.main.title("Admin Panel")
 
-        self.frame = tk.Frame(self.main, padx=10, pady=10)
-        self.frame.grid(row=0, column=0, sticky="nsew")
-        # Labels
-        tk.Label(self.frame, text="Golf Cart Plate Number:").grid(row=0, column=0, sticky="e")
-        tk.Label(self.frame, text="College:").grid(row=1, column=0, sticky="e")
+        self.frame = tk.Frame(self.main, bg="#add8e6")
+        self.frame.grid()
 
-        self.plate_number_entry = tk.Entry(self.frame)
+        font = ("Helvetica", 14)
+        welcome_font = ("Helvetica", 20, "bold")
+        light_blue = "#add8e6"
+        # Labels
+        tk.Label(self.frame, text="   Cart Creation", font=welcome_font, bg=light_blue).grid(row=0, column=0, columnspan=3, pady=30, sticky="n")
+        tk.Label(self.frame, text="Golf Cart Plate Number:", font=font, bg=light_blue).grid(row=2, column=0, sticky="e")
+        tk.Label(self.frame, text="College:", font=font, bg=light_blue).grid(row=3, column=0, sticky="e")
+
+        self.plate_number_entry = tk.Entry(self.frame, font=font)
         self.selected_college = tk.StringVar()
         colleges = ["College of Computer and Information Sciences", "College of Science", "College of Engineering",
                     "College of Business Administration"]
         self.college_menu = tk.OptionMenu(self.frame, self.selected_college, *colleges)
 
         # Grid layout for entry widgets
-        self.plate_number_entry.grid(row=0, column=1, padx=5, pady=5)
-        self.college_menu.grid(row=1, column=1, padx=5, pady=5)
+        self.plate_number_entry.grid(row=2, column=1, padx=5, pady=5)
+        self.college_menu.grid(row=3, column=1, padx=5, pady=30, ipadx=50)
 
         # Buttons
-        tk.Button(self.frame, text="Create", command=self.create_record).grid(row=2, column=0, columnspan=2, pady=7)
-        tk.Button(self.frame, text="Logout", command=self.logout).grid(row=3, column=0, columnspan=2, pady=7)
-        tk.Button(self.frame, text="Backup", command=self.backup).grid(row=4, column=0, columnspan=2, pady=7)
-
-        # Column and Row Weights to allow resizing
-        self.frame.columnconfigure(0, weight=1)
-        self.frame.columnconfigure(1, weight=1)
-        self.frame.rowconfigure(0, weight=1)
-        self.frame.rowconfigure(1, weight=1)
-        self.frame.rowconfigure(2, weight=1)
-        self.frame.rowconfigure(3, weight=1)
-        self.frame.rowconfigure(4, weight=1)
+        tk.Button(self.frame, text="Create", command=self.create_record, font=font).grid(row=4, column=1, sticky="w", pady=15)
+        tk.Button(self.frame, text="Backup", command=self.backup, font=font).grid(row=5, column=1, sticky="w", pady=10)
+        tk.Button(self.frame, text="Logout", command=self.logout, font=font).grid(row=6, column=1, sticky="w", pady=10)
 
     def create_record(self):
         # Dummy: Send information to the central database (replace with actual database interaction)     (((DONE)))
@@ -58,11 +54,19 @@ class AdminWindow:
         Login.LoginWindow(self.main)
 
     def backup(self):
-        data_to_backup = [["Plate Number", "College"],
-                          [self.plate_number_entry.get(), self.selected_college.get()]]
+        conn = sqlite3.connect("GolfDataBase.db")
+        P = "SELECT PlateNumber FROM CartData"
+        C = "SELECT College FROM CartData"
+
+        Plates = conn.execute(P).fetchall()
+        College = conn.execute(C).fetchall()
+        pairedData = zip(Plates, College)
+        data_to_backup = [["Plate Number", "College"]]
 
         with open('backup.csv', 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerows(data_to_backup)
+
+            csvwriter.writerows(pairedData)
 
         messagebox.showinfo("Backup", "Backup successful. Data saved to 'backup.csv'.")
